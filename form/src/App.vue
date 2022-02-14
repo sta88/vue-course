e4ca93a5c8731ebfac8d64de1b1e5e713700cbb1
Tanya <sta-88@mail.ru>
2022-02-14 23:47:48 +0300

    Разделение на компоненты, попытка включать второй компонент после отправки формы.

--- form/src/App.vue
+++ form/src/App.vue
@@ -1,169 +1,35 @@
 <template>
   <div class="container">
-    <h1>Form</h1>
-    <div class="col-md-4">
-      <form
-        @submit="onSubmit"
-        action="src/assets/form.json"
-        method="post">
-        
-        <p v-if="errors.length" class="text-danger">
-          <b>Пожалуйста исправьте указанные ошибки:</b>
-          <ul>
-            <li 
-              v-for="(error, idx) in errors" 
-              v-bind:key="idx">
-              {{ error }}
-            </li>
-          </ul>
-        </p>
-
-        <div class="mb-3">
-          <label for="name" class="form-label">Имя</label>
-          <input
-            id="name"
-            v-model="name"
-            type="text"
-            name="name"
-            class="form-control" 
-          >
-        </div>
-
-        <div class="mb-3">
-          <label for="phone" class="form-label">Phone</label>
-          <div class="row">
-            <div class="col-3">
-              <select 
-                v-model="code"
-                class="form-select" 
-                aria-label="Default select example">
-                <option 
-                  v-for="c in codes" 
-                  v-bind:value="c.id"
-                  :key = "c.id">
-                  {{ c.value }}
-                </option>
-              </select>
-            </div>
-            <div class="col-9">
-              <input
-                v-model="phone"
-                v-on:input="checkPhone"
-                type="phone"
-                name="phoneNumber"
-                class="form-control" 
-                id="phone" 
-                v-maska="phoneMask()">
-              <div id="phoneHelp" class="form-text">Enter your phone: {{masks[code]}}</div>
-            </div>
-          </div>
-        </div>
-
-        <div v-if="phoneChecked" class="mb-3">
-          <label for="pin" class="form-label">Код</label>
-          <input
-            id="pin"
-            v-model="pin"
-            type="text"
-            name="pin"
-            class="form-control" 
-          >
-        </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
+    <component :is="currComponent"></component>
   </div>
 </template>
 
 <script>
-export default {
-  name: "App",
+  import Register from '@/components/Register'
+  import Catalog from '@/components/Catalog'
+  export default {
+    name: "App",
 
-  data() {
-    return {
-      errors: [],
-      name: '',
-      phone: '',
-      code: 'ru',
-      codes: [
-        {id: 'ru', value: '+7'},
-        {id: 'by', value: '+375'}
-      ],
-      masks: {
-        'ru' : '###-###-##-##',
-        'by' : '###-###-###'
-      },
-      pin: '',
-      phoneChecked: false,
-      flag: true,
-    };
-  },
+    components: {
+      Register,
+      Catalog
+    },
 
-  methods: {
-    onSubmit(e) {
-      if (this.name && this.phone) {        
-        this.name = null;
-        this.phone = null;
-        
-        e.preventDefault();
+    props: 
+        ['currComp']
+    ,
 
-        let answerJson = e.target.getAttribute('action');
-        
-        fetch(answerJson)
-          .then(response => {
-            response.json();
-            console.log(response.json());
-          });
+    data() {
+      return {
+      };
+    },
 
-      } else {
-
-        this.errors = [];
-
-        if (!this.name) {
-          this.errors.push('Требуется указать имя.');
-        }
-        if (!this.phone) {
-          this.errors.push('Требуется указать телефон.');
-        }
-
-        e.preventDefault();
-      }
-
+    methods: {
     },
     
-    phoneMask() {
-      return this.masks[this.code];
-    },
-    
-    checkPhone() {
-      if (this.phone.length === this.masks[this.code].length) {
-      console.log(this.flag);
-        if (this.flag) {
-          this.axios.get('/form.json')
-            .then((response) => {
-              if (response.data.success) {
-                this.phoneChecked = true;
-              } else {
-                this.errors.push('Ошибка передачи данных.');
-              }
-            })
-            .catch((error) => {
-              this.phoneChecked = false;
-              console.log(error);
-            });
-          this.flag = false;
-        }
-      console.log('22', this.flag);
-      } else {
-        this.flag = true;
-      }
+    computed: {
     }
-  },
-  
-  computed: {
-  }
-};
+  };
 </script>
 
 <style>
---
+++ form/src/components/Catalog.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>Catalog</h2>
+</template>
+
+
+<script>
+  export default {
+    props: 
+        ['currComp']
+    ,
+
+    data() {
+      return {
+      }
+    },
+
+    methods: {      
+    }
+  };
+</script>
